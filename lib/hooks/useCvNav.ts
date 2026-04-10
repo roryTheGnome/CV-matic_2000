4ff3811a9e1943ff3126a18a58cc629cf7ed9a74
roryTheGnome <s29991@pjwstk.edu.pl>
2026-04-10 11:45:50 +0200

    fix - mini fix in cv nav id

--- lib/hooks/useCvNav.ts
+++ lib/hooks/useCvNav.ts
@@ -1,30 +1,30 @@
-import { PRIVATE_ROUTES } from "@/config/routes"
-import { usePathname } from "next/navigation"
+import { PRIVATE_ROUTES } from '@/config/routes'
+import { usePathname } from 'next/navigation'
 import { useCv } from '@/lib/hooks/cvHooks/useCv'
 
 export function useCvNav(id?: string) {
   const pathname = usePathname()
-  const {cv,isLoading,error }=useCv()
+  const { cv, isLoading, error } = useCv()
   if (!cv) {
     return { error }
   }
 
   const tabs = [
-    { label: "Details", path: `${PRIVATE_ROUTES.CVS}/${id}` },
-    { label: "Skills", path: `${PRIVATE_ROUTES.CVS}/${id}/skills` },
-    { label: "Projects", path: `${PRIVATE_ROUTES.CVS}/${id}/projects` },
-    { label: "Preview", path: `${PRIVATE_ROUTES.CVS}/${id}/preview` },
+    { label: 'Details', path: `${PRIVATE_ROUTES.CVS}/${cv.id}` },
+    { label: 'Skills', path: `${PRIVATE_ROUTES.CVS}/${cv.id}/skills` },
+    { label: 'Projects', path: `${PRIVATE_ROUTES.CVS}/${cv.id}/projects` },
+    { label: 'Preview', path: `${PRIVATE_ROUTES.CVS}/${cv.id}/preview` },
   ]
 
   const isActive = (tabPath: string) => {
     if (pathname === tabPath) return true
     if (
-      tabPath !== `${PRIVATE_ROUTES.CVS}/${id}` &&
-      pathname.startsWith(tabPath + "/")
+      tabPath !== `${PRIVATE_ROUTES.CVS}/${cv.id}` &&
+      pathname.startsWith(tabPath + '/')
     )
       return true
     return false
   }
 
-  return { cv,isLoading,error,tabs, isActive }
+  return { cv, isLoading, error, tabs, isActive }
 }
